e976092785868e76ce97cf2df1b92485076f8d08
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-10-17 10:24:24 +0200

    Postpone Sagrone service start to 1st April 2014.

--- js/init.js
+++ js/init.js
@@ -55,7 +55,7 @@ $(function () {
   };
 
   // year/month/day
-  $('#countdown').countdown('2015/2/1', function (event) {
+  $('#countdown').countdown('2015/4/1', function (event) {
     $(this).html(event.strftime(
       '<div class="countdown-section"><b>%w</b> <span>' + description.weeks + '</span> </div>' +
       '<div class="countdown-section"><b>%d</b> <span>' + description.days + '</span> </div>' +
