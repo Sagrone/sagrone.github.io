25c13a4d9802ca2d6ad80819d3a79e60d0d46f21
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-07-12 08:31:42 +0200

    Change countdown date to September 1st

--- js/init.js
+++ js/init.js
@@ -55,7 +55,7 @@ $(function () {
   };
 
   // year/month/day
-  $('#countdown').countdown('2015/7/1', function (event) {
+  $('#countdown').countdown('2015/9/1', function (event) {
     $(this).html(event.strftime(
       '<div class="countdown-section"><b>%w</b> <span>' + description.weeks + '</span> </div>' +
       '<div class="countdown-section"><b>%d</b> <span>' + description.days + '</span> </div>' +
